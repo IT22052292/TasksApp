39acddb6292156918f1e17240315a20a7cbf0794
IT22052292 <gavesha70@gmail.com>
2024-05-12 22:24:41 +0530

    final push after checking everything

--- app/src/main/java/com/example/thenotesapp/MainActivity.kt
+++ app/src/main/java/com/example/thenotesapp/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.example.thenotesapp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.example.thenotesapp.database.NoteDatabase
 import com.example.thenotesapp.repository.NoteRepository
 import com.example.thenotesapp.viewmodel.NoteViewModeFactory
 import com.example.thenotesapp.viewmodel.NoteViewModel
 
-
-
-
 class MainActivity : AppCompatActivity() {
 
     lateinit var noteViewModel: NoteViewModel
-
-    override fun onCreate(savedInstanceState:Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -23,9 +19,7 @@ class MainActivity : AppCompatActivity() {
     }
     private fun setupViewModel(){
         val noteRepository = NoteRepository(NoteDatabase(this))
-        val viewModelProviderFactory = NoteViewModeFactory(application, noteRepository)
-        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+        val viewModelProviderFactory = NoteViewModeFactory(application,noteRepository)
+        noteViewModel =ViewModelProvider(this,viewModelProviderFactory)[NoteViewModel::class.java]
     }
-
-
 }
